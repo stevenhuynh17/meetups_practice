7425abcf8de1fc546de785bffb3c41baa647b67c
Steven <stevenhuynh17@gmail.com>
2018-07-07 18:19:28 -0700

    str counting letters successful

--- 7-5_interview.js
+++ 7-5_interview.js
@@ -1,7 +1,18 @@
 // Check whether a given string can become a palindrome.
 
-function checkPalindrome() {
-  console.log("TESTING")
+function checkPalindrome(str) {
+  let obj = {}
+
+  for(let i in str) {
+    console.log(str[i])
+    if(!obj[str[i]]) {
+      obj[str[i]] = 1
+    } else {
+      obj[str[i]] += 1
+    }
+  }
+
+  console.log(obj)
 }
 
 checkPalindrome("ABCBA")
