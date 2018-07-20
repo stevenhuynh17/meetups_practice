c58b0537bbd9fac47c6319b92c57dd97ad802f05
Steven <stevenhuynh17@gmail.com>
2018-07-20 14:51:59 -0700

    done

---
+++ HackerRank/Python/arthmetic.py
@@ -0,0 +1,12 @@
+def do_math(a, b):
+    a = int(a)
+    b = int(b)
+    print a + b
+    print a - b
+    print a * b
+
+do_math(raw_input(), raw_input())
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
